64625461352e86548f722c2ab3dbd762c62cb87a
Steven Gonzalez <steven0810ange@gmail.com>
2026-03-01 12:33:00 -0500

    Validating empty data

--- src/components/Settings/DataForm.tsx
+++ src/components/Settings/DataForm.tsx
@@ -10,21 +10,48 @@ import SelectsDataForm from "./SelectsDataForm";
 
 export default function DataForm() {
   const dispatch = useAppDispatch();
-  const { user, profile, isLoading } = useAppSelector((state) => state.auth);
+  const { user, profile, preferences, isLoading } = useAppSelector(
+    (state) => state.auth,
+  );
   const capitalizeName = capitalizeWords(profile?.full_name ?? "");
 
-  const handleSubmit = async (formData: FormData) => {
-    const data = Object.fromEntries(formData) as unknown as UpdateUserProfileData;
+  const handleSubmit = async (formData: FormData, e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const data = Object.fromEntries(
+      formData,
+    ) as unknown as UpdateUserProfileData;
+    const newFullName = data.full_name?.trim() || "";
+    const newLanguage = data.language || "";
+    const newTimezone = data.timezone || "";
+
+    const hasChanges =
+      (newFullName && newFullName !== profile?.full_name) ||
+      (newLanguage && newLanguage !== preferences?.language) ||
+      (newTimezone && newTimezone !== preferences?.timezone);
 
     if (!data.full_name || !data.language || !data.timezone) {
       console.error("Faltan campos requeridos");
       return;
     }
     try {
+      if (!hasChanges) {
+        addToast({
+          title: "No hay cambios para guardar",
+          timeout: 5000,
+          color: "warning",
+          icon: "Info",
+        });
+        return;
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const result = await dispatch(updateUserData(data)).unwrap();
-      addToast({ title: "Datos actualizados", color: "success", icon: "Check" });
-    } 
-    catch (rejectedValueOrError) {
+      addToast({
+        title: "Datos actualizados",
+        color: "success",
+        icon: "Check",
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (rejectedValueOrError) {
       addToast({ title: "Error al actualizar datos", color: "danger" });
     }
   };
@@ -35,7 +62,7 @@ export default function DataForm() {
   return (
     <Form
       className="w-full flex flex-col items-center justify-center sm:w-1/2 lg:w-1/3"
-      onSubmit={(e) => handleSubmit(new FormData(e.currentTarget))}
+      onSubmit={(e) => handleSubmit(new FormData(e.currentTarget), e)}
     >
       <Input
         label="Nombre completo"
