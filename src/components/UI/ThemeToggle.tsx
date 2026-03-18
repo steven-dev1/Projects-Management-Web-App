6b92a2917258121f61a1289fb7aca0e577178c90
Steven Gonzalez <steven0810ange@gmail.com>
2026-03-18 00:33:56 -0500

    [Fix] Modo oscuro en páginas publicas

--- src/components/UI/ThemeToggle.tsx
+++ src/components/UI/ThemeToggle.tsx
@@ -2,7 +2,7 @@
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 
-export function ThemeToggle() {
+export function ThemeToggle({ iconOnly = false }: { iconOnly?: boolean }) {
   const { theme, setTheme } = useTheme();
   return (
     <button
@@ -10,7 +10,27 @@ export function ThemeToggle() {
       className="cursor-pointer w-full text-center flex gap-2 items-center"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-       {theme === "dark" ? <>Modo claro <Sun size={18} /></> : <>Modo oscuro <Moon size={18} /></>} 
+      {theme === "dark" ? (
+        <>
+          {iconOnly ? (
+            <Moon size={18} />
+          ) : (
+            <>
+              Modo claro <Sun size={18} />
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          {iconOnly ? (
+            <Sun size={18} />
+          ) : (
+            <>
+              Modo oscuro <Moon size={18} />
+            </>
+          )}
+        </>
+      )}
     </button>
   );
 }
